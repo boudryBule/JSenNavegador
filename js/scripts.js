19d5bd8767157e75f3dacb2fbf909fe654ecca61
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-02 16:59:04 +0100

    seleccionar elementos con querySelectorAll y getElementById

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,36 @@ heading.textContent = 'nuevo heading';
 
 //también puedes añadir nuevas clases desde js 
 //heading.classList.add('nueva-clase');
+
+//querySelectorAll -> devuelve ninguno o todos los elementos que concuerden con el selector que se le mande
+
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces);
+//para acceder a uno en concreto es igual que con los arrays
+console.log(enlaces[1]);
+
+//para cambiar el texto
+enlaces[0].textContent = 'Nuevo texto para enlace';
+
+//también podrías hacerlo todo en una linea
+// document.querySelectorAll('.navegacion a')[0].textContent = 'Nuevo texto para enlace';
+
+//también puedes cambiar el enlace propiamente dicho ( a dónde te manda)
+
+// enlaces[0].href = 'https://google.com';
+
+//también puedes añadirle una clase nueva
+
+enlaces[0].classList.add('Nueva-clase');
+
+//también eliminarlas
+
+enlaces[0].classList.remove('navegacion__enlace');
+
+
+
+
+//getElementById esta forma es antigua y ya no se usa
+
+const heading2 = document.getElementById('heading');
+console.log(heading2);
