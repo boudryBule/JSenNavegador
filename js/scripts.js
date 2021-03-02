e5372808d66c146eb1baff70c3e1862be7cbce97
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-02 17:17:45 +0100

    crear HTML con createElement

--- js/scripts.js
+++ js/scripts.js
@@ -37,7 +37,7 @@ enlaces[0].classList.add('Nueva-clase');
 
 //también eliminarlas
 
-enlaces[0].classList.remove('navegacion__enlace');
+// enlaces[0].classList.remove('navegacion__enlace');
 
 
 
@@ -46,3 +46,25 @@ enlaces[0].classList.remove('navegacion__enlace');
 
 const heading2 = document.getElementById('heading');
 console.log(heading2);
+
+//generar nuevo enlace
+
+const nuevoEnlace = document.createElement('A') //hay que pasarle la etiqueda en mayusculas
+
+//Agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+
+//agregar el texto
+nuevoEnlace.textContent = 'Un nuevo enlace';
+
+//agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+//agregarlo al documento 
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+
+
+
+console.log(nuevoEnlace);
